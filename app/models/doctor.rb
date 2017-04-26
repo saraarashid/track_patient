9a2d9045fd4f90e3a4ac90c6dde7b0d19c205ca7
Saraa <saraa_rashid@yahoo.com>
2017-04-26 12:06:42 +0000

    validation

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,5 +1,8 @@
 class Doctor < ActiveRecord::Base
     has_many :patients
-   
+validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@gmail\.com\z/,
+                  message: "must be a gmail.com account" }
+validates_uniqueness_of :license_no , case_sensitive: true
+validates_format_of :phone_number, with:  /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/
 end 
 
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,7 @@
 class Patient < ActiveRecord::Base
     belongs_to :doctor
-    
+validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@gmail\.com\z/,
+                  message: "must be a gmail.com accounts" }
+validates_uniqueness_of :first_name, :last_name , :doctor_id, case_sensitive: true
+validates_format_of :phone_number, with:  /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/
 end
